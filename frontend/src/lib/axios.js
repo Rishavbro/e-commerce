5659e7eae4f8edaa24afe0df319598358e1600b0
Rishavbro <kumarrishavsingh125@gmail.com>
2025-04-21 10:22:24 +0530

    completed frontend

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL:'http://localhost:3000/api',
-    withCredentials:true,
-})
+	baseURL: import.meta.mode === "development" ? "http://localhost:3000/api" : "/api",
+	withCredentials: true, // send cookies to the server
+});
 
 export default axiosInstance;
